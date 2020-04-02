d7725be511182c61abef7a7065f0113f3d4b68c0
John Nagle <johnnagleiv@gmail.com>
2020-04-02 18:19:32 -0400

    Black and white color scheme

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Liberation Mono:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -83,47 +83,44 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 0.95;
 
 /* Terminal colors (16 first used in escape sequence) */
-static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+const char *colorname[] = {
+    /* 8 normal colors */
+    [0] = "#080303", /* black   */
+    [1] = "#635B5A", /* red     */
+    [2] = "#67605F", /* green   */
+    [3] = "#736D6D", /* yellow  */
+    [4] = "#827D7C", /* blue    */
+    [5] = "#84807F", /* magenta */
+    [6] = "#918D8D", /* cyan    */
+    [7] = "#cdcccd", /* white   */
 
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+    /* 8 bright colors */
+    [8]  = "#8f8e8f",  /* black   */
+    [9]  = "#635B5A",  /* red     */
+    [10] = "#67605F", /* green   */
+    [11] = "#736D6D", /* yellow  */
+    [12] = "#827D7C", /* blue    */
+    [13] = "#84807F", /* magenta */
+    [14] = "#918D8D", /* cyan    */
+    [15] = "#cdcccd", /* white   */
 
-	[255] = 0,
+    [255] = 0,
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"black",
+    /* special colors */
+    [256] = "#080303", /* background */
+    [257] = "#cdcccd", /* foreground */
+    [258] = "#cdcccd",     /* cursor */
 };
 
-
-/*
- * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
- */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 258;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+/* Default colors (colorname index)
+ * foreground, background, cursor */
+ unsigned int defaultbg = 0;
+ unsigned int defaultfg = 257;
+ unsigned int defaultcs = 258;
+ unsigned int defaultrcs= 258;
 
 /*
  * Default shape of cursor
